f50c38f06da7c58c33fda0f41c1c45da90f93629
kikepriet <37045937+kikepriet@users.noreply.github.com>
2021-07-22 17:01:43 -0500

    Update conexion.php
    Cambio de datos para base de datos

--- db/conexion.php
+++ db/conexion.php
@@ -1,7 +1,7 @@
 <?php 
-$host = "localhost";
-$user = "root"; //"id16989109_kikepriet";
-$pass ="JEQE*ub841";  //"Wendy-trinatural2021";
-$db ="trinatural";  //"id16989109_trinatural";
+$host = "your_host";
+$user = "your_name";
+$pass ="your_password";
+$db ="your_database";
 $conexion = mysqli_connect($host, $user, $pass, $db);
 ?>
